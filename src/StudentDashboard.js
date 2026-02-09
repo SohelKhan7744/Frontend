f20017176ce8b132bf0c48707d1db253b407c745
Sohel Khan <khansohel7744@gmail.com>
2026-02-09 15:17:37 +0530

    lets check

--- src/StudentDashboard.js
+++ src/StudentDashboard.js
@@ -10,6 +10,49 @@ function StudentDashboard() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   
+const SkeletonBox = ({className})=>(
+<div className ={`animate-pulse bg-gray-700 rounded ${className}`}/>
+)
+   const StudentDashboardSkeleton = () =>
+   {
+     return (
+         <div className="p-8 text-white space-y-6">
+          {/* Header */}
+            <SkeletonBox className="h-8 w-64" />
+            {/* profile Card */}
+            <div className="bg-gray-800 p-4 rounded-lg space-y-3">
+           <SkeletonBox className="h-4 w-32"/>
+           <SkeletonBox className="h-4 w-40"/>
+           <SkeletonBox className="h-4 w-48"/> 
+            </div>
+            {/* All Courses */}
+
+            <SkeletonBox className="h-6 w-40"/>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              {[...Array(4)].map((_,i) => (
+                <div key={i}
+                className="bg-gray-700 p-4 rounded-lg space-y-3">
+                <SkeletonBox className="h-5 w-3/4"/>
+                <SkeletonBox className="h-4 w-full"/>
+                <SkeletonBox className="h-9 w-28 mt-3"/>
+                </div>
+              ))}
+            </div>
+            {/* My Courses */}
+            <SkeletonBox className="h-6 w-36 mt-6"/>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {[...Array(2)].map((_,i) => (
+            <div key={i}
+            className="bg-gray-700 p-4 rounded-lg space-y-3">
+             <SkeletonBox className="h-5 w-3/4"/>  
+             <SkeletonBox className="h-4 w-full"/>
+            </div>
+          ))}
+            </div>
+
+         </div>
+     )
+   }
 
   useEffect(() => {
     LoadData();
@@ -47,7 +90,7 @@ function StudentDashboard() {
   };
 
   if (loading) {
-    return <p className="text-white text-center mt-10">Loading...</p>;
+    return <StudentDashboardSkeleton/>;
   }
 
   if (error) {
