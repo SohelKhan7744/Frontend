18b2aba20e02d18cb050b1b13d5e3c7a33d3dad1
Sohel Khan <khansohel7744@gmail.com>
2026-02-11 01:54:00 +0530

    Refactor AdminLayout and App components for improved structure and styling

--- src/AdminOutlat.js
+++ src/AdminOutlat.js
@@ -1,17 +1,29 @@
 import { Outlet } from "react-router-dom";
+import Menu from "./Menu";
 
 function AdminLayout() {
   return (
-    <div className="content-wrapper">
-  <section className="content">
-    <div className="px-4 md:px-6 py-4">
-      <Outlet />
-    </div>
-  </section>
-</div>
+    <div className="wrapper">
+      {/* AdminLTE Navbar */}
+      <nav className="main-header navbar navbar-expand navbar-dark">
+        {/* you already have this in AdminLTE */}
+      </nav>
 
+      {/* AdminLTE Sidebar */}
+      <aside className="main-sidebar sidebar-dark-primary elevation-4">
+        <Menu/>
+      </aside>
+
+      {/* REQUIRED */}
+      <div className="content-wrapper">
+        <section className="content">
+          <div className="container-fluid px-4 py-4">
+            <Outlet />
+          </div>
+        </section>
+      </div>
+    </div>
   );
 }
-
 
 export default AdminLayout;
--- src/App.js
+++ src/App.js
@@ -19,29 +19,29 @@ function App() {
   return (
     <>
       <Routes>
-        {/* PUBLIC ROUTES */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+  {/* public routes */}
+  <Route path="/login" element={<Login />} />
+  <Route path="/register" element={<Register />} />
 
-        {/* ADMIN ROUTES */}
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute>
-              <AdminLayout />
-            </ProtectedRoute>
-          }
-        >
-          <Route path="dashboard" element={<DashboardPage />} />
-          <Route path="student" element={<Students />} />
-          <Route path="student/edit/:id" element={<EditStudent />} />
-          <Route path="teacher" element={<Teachers />} />
-          <Route path="teacher/edit/:id" element={<EditTeacher />} />
-          <Route path="teacher/assign/:username" element={<AssignTeacherToCourse />} />
-          <Route path="courses" element={<Courses />} />
-          <Route path="courses/edit/:id" element={<EditCourse />} />
-        </Route>
-      </Routes>
+  {/* ADMIN ROUTES */}
+  <Route
+    path="/admin"
+    element={
+      <ProtectedRoute>
+        <AdminLayout />
+      </ProtectedRoute>
+    }
+  >
+    <Route path="dashboard" element={<DashboardPage />} />
+    <Route path="student" element={<Students />} />
+    <Route path="student/edit/:id" element={<EditStudent />} />
+    <Route path="teacher" element={<Teachers />} />
+    <Route path="teacher/edit/:id" element={<EditTeacher />} />
+    <Route path="teacher/assign/:username" element={<AssignTeacherToCourse />} />
+    <Route path="courses" element={<Courses />} />
+    <Route path="courses/edit/:id" element={<EditCourse />} />
+  </Route>
+</Routes>
 
       <ToastContainer
         position="top-right"
