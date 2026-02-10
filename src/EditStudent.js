6bb9e59692f0e270af6da303bdd5542f56c334a8
Sohel Khan <khansohel7744@gmail.com>
2026-02-10 14:32:24 +0530

    Fix import typo for EditSkeleton component in EditStudent

--- src/EditStudent.js
+++ src/EditStudent.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import api from "./axios";
-import EditSkeleton from "./EditSkeleton";
+import EditSkeleton from "./EditSkeliton";
 
 function EditStudent() {
   const { id } = useParams();
